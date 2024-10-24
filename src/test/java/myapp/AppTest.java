f430ac6bdd1231207098ac19535d84ba7ee71f7e
fazin <fazinashraf35@gmail.com>
2024-10-24 11:21:29 +0530

    hjm

--- src/test/java/myapp/AppTest.java
+++ src/test/java/myapp/AppTest.java
@@ -1,6 +1,7 @@
 package myapp;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -16,4 +17,13 @@ public class AppTest {
     public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
+
+    /**
+     * Example Test for Addition
+     */
+    @Test
+    public void testExample() {
+        assertEquals(5, 2 + 3);
+    }
 }
+
