d269d10f2f50ded31ff8bf3f3c7986ffb0e61112
fazin <fazinashraf35@gmail.com>
2024-10-24 10:52:11 +0530

    Thirdcommit

--- file1.py
+++
@@ -1,7 +0,0 @@
-# test_sum.py
-
-def sum_of_two_numbers(a, b):
-    return a + b
-
-def test_sum():
-    assert sum_of_two_numbers(2, 3) == 5, "Test failed: 
---
+++ src/main/java/package app/Sample.java
@@ -0,0 +1,10 @@
+package 1;
+
+/**
+ * Hello world!
+ */
+public class Sample {
+    public static void main(String[] args) {
+        System.out.println("Fazin!");
+    }
+}
